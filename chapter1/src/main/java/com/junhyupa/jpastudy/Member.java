074a951128b7d2ff1066f7c7fc1e4bd8a54cb902
jun <yoseb95@gmail.com>
2024-04-17 18:32:54 +0900

    Fix: chapter2, chpater4 버전 업그레이드

---
+++ chapter1/src/main/java/com/junhyupa/jpastudy/Member.java
@@ -0,0 +1,6 @@
+package com.junhyupa.jpastudy;
+
+public class Member {
+    private String memberId;
+    private String name;
+}
---
+++ chapter1/src/main/java/com/junhyupa/jpastudy/MemberDAO.java
@@ -0,0 +1,5 @@
+package com.junhyupa.jpastudy;
+
+public class MemberDAO {
+    public Member find(String memberId) {}
+}
